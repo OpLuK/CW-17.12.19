83d37a53778adb2217ee5d5f17d0f177a5ad605b
Vitaliy <55364824+OpLuK@users.noreply.github.com>
2019-12-17 15:54:10 +0300

    Add files via upload

---
+++ Mod dostupa/src/class1.java
@@ -0,0 +1,30 @@
+public class class1 {
+	public String FIO;
+	protected String date;
+	private String pass;
+	private int snils;
+	void filimonov (){
+		FIO = "Филимонов Иван Геннадьевич";
+		date = "1.1.2001";
+		pass = "1212 123123";
+		snils = 234546745;
+	}
+	void moshnin (){
+		FIO = "Мошнин Кирилл Никитович";
+		date = "2.2.2001";
+		pass = "1313 123124";
+		snils = 435644234;
+	}
+	void zankisov (){
+		FIO = "Занкисов Давид Владимирович";
+		date = "3.3.2002";
+		pass = "1414 312312";
+		snils = 123533546;
+	}
+	void kiyko (){
+		FIO = "Кийко Диана Дмитриевна";
+		date = "4.4.2001";
+		pass = "1415 345345";
+		snils = 123455346;
+	}
+}
